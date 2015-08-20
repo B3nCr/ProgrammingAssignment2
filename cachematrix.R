1e631f33775ed2feb251ccc5bfc7cf7980b4b35a
Ben Crinion <ben.crinion@gaist.co.uk>
2015-08-20 12:54:28 +0100

    Implemented cache object and cache solve function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# makeCacheMatrix returns an object which will cache a matrix and it's inverse
 makeCacheMatrix <- function(x = matrix()) {
+    my_inverse <- NULL
 
+    # function to return the cached matrix
+    getMatrix <- function() { x }
+
+    # updates matrix to new value and clears cache
+    setMatrix <- function(m) {
+        print('clearing cache')
+
+        ## double arrow <<- sets the variable on the parent scope
+        ## that means this set x, the "plain" matrix, to the new value m
+        x          <<- m
+        # and clears my_inverse, the cached inverse
+        my_inverse <<- NULL
+    }
+
+    # returns the cached inverse
+    getInverse <- function(){ my_inverse }
+
+    ## caches the inverse supplied by i in the parent scope
+    setInverse <- function(i) { my_inverse <<- i }
+
+    # return list of functions
+    makeCacheMatrix <- list(getMatrix = getMatrix , setMatrix = setMatrix, getInverse = getInverse, setInverse = setInverse)
 }
 
 
-## Write a short comment describing this function
-
+## takes a cache object (makeCacheMatrix) and gets the inverse from the cache if it's available
+## or calculates the inverse and adds it back to the cache object for next time around
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## get the cached value
+    inverse <- x$getInverse()
+
+    # check if it's not null
+    if(!is.null(inverse)){
+        print('using cached inverse')
+
+        # return the cached inverse
+        cacheSolve <- inverse
+
+        # stop processsing
+        return
+    }
+
+    # cache is null so get the matrix and store in m
+    m <- x$getMatrix()
+
+    # invert the matrix and store the result in i
+    i <- solve(m)
+
+    # push the inverse back into the cache
+    x$setInverse(i)
+
+    # return the inverse
+    cacheSolve <- i
 }
